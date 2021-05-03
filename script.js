ec1309d174216d4b964c9dda71d258887c006a2e
Daniel Kim <kkdh1109@gmail.com>
2021-05-02 22:31:59 -0400

    add blueprint for a cell object and create a fxn that cycles thr the grid and inserting cell object to grid arr

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ const ctx = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 600;
 
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
+
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+
+class Cell {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw() {
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function createGrid() {
+    for (let y = cellSize; y < canvas.height; y += cellSize) {
+        for (let x = 0; x < canvas.width; x += cellSize) {
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+
+function animate() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
+    requestAnimationFrame(animate);
+}
+
+animate();
